750d239fdda8d4eec290a06cce50adc0d6b0cc7d
Offiongwhilley <offiongwhilley@gmail.com>
2022-03-10 04:39:40 -0800

    modification

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 
-#include <unistd.h>
-
 
 
 /**
+ * main - prints a sentence followed by a new line
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *       */
+ * Return: returns 1 after printing 
+ */
 
 
 
@@ -24,7 +14,9 @@ int main(void)
 
 {
 
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+
 
 			return (1);
 

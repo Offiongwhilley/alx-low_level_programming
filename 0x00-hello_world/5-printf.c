10335af3defc863a16f89f2025dd0c62dd8fc702
Offiongwhilley <offiongwhilley@gmail.com>
2022-03-10 04:22:08 -0800

    modified file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
+
+
 /**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
-*/
+ * main - prints a sentence with the printf function
+ *
+ * Return: returns 0 after printing
+ */
 
 int main(void)
 
 {
 
-		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
 

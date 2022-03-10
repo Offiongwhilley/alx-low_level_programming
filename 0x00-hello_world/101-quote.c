7285398bfd83887b2c5aada4530f4759c9aa2fc3
Offiongwhilley <offiongwhilley@gmail.com>
2022-03-10 04:56:47 -0800

    modified file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 
-
-
 /**
- * main - prints a sentence followed by a new line
- *
- * Return: returns 1 after printing 
- */
-
-
+  * main - prints a sentence followed by a new line
+  *
+  * Return: returns 1 after printing
+  */
 
 int main(void)
-
 {
-
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-
-
-			return (1);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
 }
+
+

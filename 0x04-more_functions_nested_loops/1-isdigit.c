b73522843eedd7530d3e423ec53ede20f7dbfc29
Offiongwhilley <offiongwhilley@gmail.com>
2022-03-17 03:26:23 -0700

    added tasks files

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if parameter is a number between 0 to 9.
+ * @c: input number.
+ * Return: 1 if c is a number (0 to 9), 0 in other case.
+ */
+
+int _isdigit(int c)
+
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This file contains more functions and nested loops

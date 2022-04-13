b397e3d1e69904dc1aca72e0be9313d06aa2ebda
Offiongwhilley <offiongwhilley@gmail.com>
2022-04-13 04:38:36 -0700

    added advanced task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,9 +5,9 @@
  * main - check the code for Holberton School students.
  * @argc: argument count.
  * @argv: argument vector.
- *
  * Return: Always 0.
  */
+
 int main(int argc, char *argv[])
 {
 	char *opc = (char *) main;

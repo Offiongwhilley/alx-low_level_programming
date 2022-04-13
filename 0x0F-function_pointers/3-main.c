bc13b1e3eef5dea773eeb14e4c545987c1dde0ac
Offiongwhilley <offiongwhilley@gmail.com>
2022-04-13 11:28:46 -0700

    modified task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,40 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 
 /**
- * main - check the code for Holberton School students.
- * @argc: argument count.
- * @argv: argument vector.
- * Return: Always 0.
+ * main - entry point for function
+ * @argc: arg counter, number of args
+ * @argv: array of pointers to args
+ * Description: calculates basic arithmetic
+ * Return: 0 on success, 98 on failure
  */
 
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int (*operation)(int, int);
+	char op;
+	int (*op_func)(int, int);
+	int result;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
-
-	if (argv[2][1])
-	{
+	op = *(argv[2]);
+	if ((op == '/' || op == '%') && !atoi(argv[3]))
+	{/* divide by zero exception */
 		printf("Error\n");
-		exit(99);
+		return (100);
 	}
-
-	operation = get_op_func(argv[2]);
-
-	if (operation == NULL)
-	{
+	/* actually do the operation now that we've error chcked */
+	op_func = get_op_func(&op);
+	if (op_func == NULL || argv[2][1] != '\0')
+	{ /* didn't find operator, or operator longer than 1 byte */
 		printf("Error\n");
-		exit(99);
+		return (99);
 	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	printf("%d\n", operation(a, b));
+	result = op_func(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", result);
 	return (0);
 }
